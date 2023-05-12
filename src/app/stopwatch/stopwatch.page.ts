c4f28a195bdecd3ef90f06bdbff5b890e7d02a3a
LukeCorcoran99 <lukecorc@gmail.com>
2023-05-12 02:01:41 +0100

    final commit

--- src/app/stopwatch/stopwatch.page.ts
+++ src/app/stopwatch/stopwatch.page.ts
@@ -7,34 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StopwatchPage implements OnInit {
 
-  stopwatch: number = 0; //holds and displays the stopwatch timer
-  timer: any = null; //stores setInterval function, resets to null when timer is stopped
 
+  stopwatch = { hours: 0, minutes: 0, seconds: 0, milliseconds: 0 }; // holds the stopwatch timer, to be formatted later
+
+  timer: any = null; // stores setInterval function, resets to null when timer is stopped
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
 
   start() {
-    //This if statement starts the timer if it is not already running.
+       //This if statement starts the timer if it is not already running.
     // If the timer is null it will execute, and it sets the timer to the setInterval function 
     if (!this.timer) {
-      this.timer = setInterval(() => {this.stopwatch++;}, 1000); //Function that increments the stopwatch value every 1000ms (1s)
+      this.timer = setInterval(() => {
+        this.stopwatch.milliseconds++;
+        //Resets back to 0 once it reaches 60
+        if (this.stopwatch.milliseconds === 100) {
+          this.stopwatch.seconds++;
+          this.stopwatch.milliseconds = 0;
+        }
+        if (this.stopwatch.seconds === 60) {
+          this.stopwatch.minutes++;
+          this.stopwatch.seconds = 0;
+        }
+        if (this.stopwatch.minutes === 60) {
+          this.stopwatch.hours++;
+          this.stopwatch.minutes = 0;
+        }
+      }, 10);
+    }
   }
-}
 
+  // clears the interval function, sets timer to null
   pause() {
-    // clears the interval function, sets timer to null
     clearInterval(this.timer);
     this.timer = null;
   }
 
+  // Resets all stopwatch values to 0, cleard the interval, sets timer to null
   reset() {
-    // Reset the stopwatch value to 0, cleard the interval, sets timer to null
-    this.stopwatch = 0;
+    this.stopwatch = { hours: 0, minutes: 0, seconds: 0, milliseconds: 0 };
     clearInterval(this.timer);
     this.timer = null;
   }
+
+  //formats using the padStart function
+  //Time is defined as a number in order for it to take stopwatch as argument
+  //This then converts the arguments to a string and outputs it formatted
+  formatTime(time: number): string {
+  return time.toString().padStart(2, '0');
+  }
+
+
+  //Uses formatTime function to format the milliseconds, 
+  //seconds, minutes and hours, and outputs to html formatted
+  stopwatchFormat(): string {
+    let hours = this.formatTime(this.stopwatch.hours);
+    let minutes = this.formatTime(this.stopwatch.minutes);
+    let seconds = this.formatTime(this.stopwatch.seconds);
+    let milliseconds = this.formatTime(this.stopwatch.milliseconds);
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  }
+
 
 }
